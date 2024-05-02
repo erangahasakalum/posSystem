1fd4bca1f05e28124911da04236686772a347624
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-05-02 10:28:42 +0530

    spa branch some changes done

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,9 +28,113 @@ $('#sign-in-btn').on('click', () => {
     $("#header").css({
         display:'block'
     })
-    $("#sign-up-form").css({
+    $("#dashboard-form").css({
+        display:'block'
+    })
+    $("#sign-in-form").css({
         display:'none'
     })
+})
+
+$('#dashboard-click').on('click', () => {
+    console.log("click")
+    $("#dashboard-form").css({
+        display:'block'
+    })
+
+    $("#customer-form").css({
+        display: 'none'
+    })
+
+    $("#item-form").css({
+        display:'none'
+    })
+
+    $("#order-form").css({
+        display:'none'
+    })
+
+})
+
+$("#customer-click").on('click',()=>{
+    console.log("clicked")
+    $("#customer-form").css({
+        display:'block'
+    })
+
+    $("#dashboard-form").css({
+        display:'none'
+    })
+
+    $("#item-form").css({
+        display:'none'
+    })
+
+    $("#order-form").css({
+        display:'none'
+    })
+})
+
+$("#item-click").on('click',()=>{
+    console.log("clicked")
+    $("#item-form").css({
+        display:'block'
+    })
+    $("#customer-form").css({
+        display:'none'
+    })
+
+    $("#dashboard-form").css({
+        display:'none'
+    })
+
+    $("#order-form").css({
+        display:'none'
+    })
+})
+
+$("#order-click").on('click',()=>{
+    console.log("clicked")
+    $("#order-form").css({
+        display:'block'
+    })
+    $("#item-form").css({
+        display:'none'
+    })
+    $("#customer-form").css({
+        display:'none'
+    })
+
+    $("#dashboard-form").css({
+        display:'none'
+    })
+
+})
+
+$("#log-out-click").on('click',()=>{
+    console.log("clicked")
+    $("#sign-in-form").css({
+        display:'block'
+    })
+
+    $("#order-form").css({
+        display:'none'
+    })
+    $("#item-form").css({
+        display:'none'
+    })
+    $("#customer-form").css({
+        display:'none'
+    })
+
+    $("#dashboard-form").css({
+        display:'none'
+    })
+
+    $("#header").css({
+        display:'none'
+    })
+
 })
 
 
