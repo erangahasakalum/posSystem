1cf93a38ad959e4fbd8350dfc1a6b6a5f40e4c91
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-05-31 19:04:48 +0530

    customer form done without validation

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -2,79 +2,17 @@ import CustomerModel from "../model/CustomerModel.js";
 import {customerArray} from "../db/db.js";
 
 var recodeIndex;
-// save customer
-/*$("#customer-save").on('click', () => {
-    console.log('clicked')
-    saveCustomer();
-});
-
-$('#customer-update').on('click',()=>{
-    updateCustomer();
-});
-
-function saveCustomer(){
-    console.log("customer method")
-    let customer_id = $("#customer_id").val();
-    let customer_name = $("#customer_name").val();
-    let city = $("#city").val();
-    let telephone = $("#telephone").val();
-
-    let customerObj = new CustomerModel(
-        customer_id,
-        customer_name,
-        city,
-        telephone
-    );
-    customerArray.push(customerObj);
-    loadTable();
-}
-
-function loadTable() {
-    $("#customer-table").empty();
-
-    customerArray.map((item) => {
-        let customerRecode = `<tr> 
-            <td>${item.customer_id}</td>
-            <td>${item.customer_name}</td>
-            <td>${item.city}</td>
-            <td>${item.telephone}</td>
-        </tr>`
-
-        $("#customer-table").append(customerRecode);
-
-    });
-}
-
-function updateCustomer(){
-        let customer_id = $("#customer_id").val();
-        let customer_name = $("#customer_name").val();
-        let city = $("#city").val();
-        let telephone = $("#telephone").val();
-
-        let customerObj = customerArray[recodeIndex];
-
-        customerObj.id = customer_id;
-        customerObj.name =customer_name;
-        customerObj.city =city;
-        customerObj.telephone = telephone;
-
-        console.log(customerArray[recodeIndex])
-
-        loadTable();
-}
-
-$('#customer-table-tbody').on('click','tr',function (){
-        let index = $(this).index();
-        recodeIndex =index;
-
-        console.log(recodeIndex);
-})*/
-
 $('#customer-save').on('click',()=>{
    saveCustomer();
 })
 
+$('#customer-update').on('click',()=>{
+    updateCustomer();
+})
 
+$('#delete-customer').on('click',()=>{
+    deleteCustomers();
+})
 
 function saveCustomer() {
     let customer_id = $('#customer_id').val();
@@ -91,6 +29,7 @@ function saveCustomer() {
 
     customerArray.push(customerObj);
     loadCustomer();
+    clearSaveFields();
 }
 
 function loadCustomer(){
@@ -98,14 +37,70 @@ function loadCustomer(){
 
     customerArray.map((item)=>{
         let customerRecodes = `<tr> 
-            <td>${item.customer_id}</td>
-            <td>${item.customer_name}</td>
-            <td>${item.city}</td>
-            <td>${item.telephone}</td>
+            <td class="c-id">${item.customer_id}</td>
+            <td class="c-name">${item.customer_name}</td>
+            <td class="c-city">${item.city}</td>
+            <td class="c-telephone">${item.telephone}</td>
         </tr>`
         $('#customer-table-tbody').append(customerRecodes);
     })
-
 }
 
+$('#customer-table').on('click','tr',function (){
+    let idC = $(this).find('.c-id').text();
+    let nameC =$(this).find('.c-name').text();
+    let cityC =$(this).find('.c-city').text();
+    let telephoneC =$(this).find('.c-telephone').text();
 
+    recodeIndex = $(this).index();
+
+    $("#c_id").val(idC);
+    $("#c_name").val(nameC);
+    $("#c_city").val(cityC);
+    $("#c_telephone").val(telephoneC);
+
+    $('#delete-id').val(idC);
+    $('#delete-name').val(nameC);
+})
+
+function updateCustomer() {
+    let idU = $('#c_id').val();
+    let nameU = $('#c_name').val();
+    let cityU = $('#c_city').val();
+    let telephoneU = $('#c_telephone').val();
+
+    let customerArrayElement = customerArray[recodeIndex];
+    customerArrayElement.customer_id =idU;
+    customerArrayElement.customer_name =nameU;
+    customerArrayElement.city =cityU;
+    customerArrayElement.telephone =telephoneU;
+
+    loadCustomer();
+    clearUpdateFields();
+}
+
+function clearSaveFields(){
+    $('#customer_id').val('');
+    $('#customer_name').val('');
+    $('#city').val('');
+    $('#telephone').val('');
+}
+
+function clearUpdateFields() {
+    $('#c_id').val('');
+    $('#c_name').val('');
+    $('#c_city').val('');
+    $('#c_telephone').val('');
+}
+
+function deleteCustomers() {
+    customerArray.splice(recodeIndex,1);
+    loadCustomer();
+    clearDeleteFields();
+}
+
+function clearDeleteFields() {
+    $('#delete-customer-modal.').css({
+        display:'none'
+    })
+}
